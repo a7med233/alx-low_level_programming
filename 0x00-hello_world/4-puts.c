43d30b83a6226ab57ad09112df691c1c0d7eaf92
a7med233 <ahmedemad23@mailbox.unideb.hu>
2023-03-16 20:38:00 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle/n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
