589e989a78a408f582c6f7d588f2b0556add24ef
a7med233 <ahmedemad23@mailbox.unideb.hu>
2023-03-21 22:27:23 +0100

    ahmed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - a simple program that outputs "-putchar"
+ * main - a simple program that outputs "_putchar"
  * Return: 0 on success
  */
 int main(void)
 {
-	char str[] = "-putchar\n";
+	char str[] = "-_putchar\n";
 	int i = 0;
 
 	while (str[i] != '\0')
